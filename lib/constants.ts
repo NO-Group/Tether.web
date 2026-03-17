98f2f5aa18369654df14a6f177ffe8d50ed55082
NO-Group <n.ogroup@yahoo.com>
2026-03-17 23:14:54 +0100

    Create constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+// lib/constants.ts
+export const NAV_LINKS = [
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
+  { href: "/features", label: "Features" },
+  { href: "/download", label: "Download" },
+  { href: "/early", label: "Early Access" }
+];
