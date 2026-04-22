361933550357ca778ae413b6aeb13bbef20b9717
NO-Group <n.ogroup@yahoo.com>
2026-04-22 22:25:09 +0100

    Delete next.config.jsl

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",            // make it a static site
-  images: { unoptimized: true }, // allow images
-  basePath: "/Tether.web",     // your repo name
-  assetPrefix: "/Tether.web/", // fix paths
-};
-
-module.exports = nextConfig;
