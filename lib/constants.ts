e3b70de674342cbdd0e918e6bfe8600fe2d63dde
NO-Group <n.ogroup@yahoo.com>
2026-04-22 22:37:07 +0100

    Delete lib directory

--- lib/constants.ts
+++
@@ -1,8 +0,0 @@
-// lib/constants.ts
-export const NAV_LINKS = [
-  { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
-  { href: "/features", label: "Features" },
-  { href: "/download", label: "Download" },
-  { href: "/early", label: "Early Access" }
-];
